9bb6cf13873d06b43b3a51441d5425795c9bcd42
AndyDLP <andydlp93@gmail.com>
2019-03-14 19:58:43 +0000

    allow null tests

--- Tests/appveyor.pester.ps1
+++ Tests/appveyor.pester.ps1
@@ -91,86 +91,90 @@ if(-not $Finalize) {
     # https://dille.name/blog/2017/06/29/code-coverage-metrics-using-pester-for-powershell-modules/
     $TestResults = Invoke-Pester -Path ".\Tests" -CodeCoverage ".\*\ECI-Utilities\*\*.ps1" -PassThru -Show None
 
-    $CodeCoverage = @{
-        Functions = @{}
-        Statement = @{
-            Analyzed = $TestResults.CodeCoverage.NumberOfCommandsAnalyzed
-            Executed = $TestResults.CodeCoverage.NumberOfCommandsExecuted
-            Missed   = $TestResults.CodeCoverage.NumberOfCommandsMissed
-            Coverage = 0
-        }
-        Function = @{}
-    }
-    $CodeCoverage.Statement.Coverage = [math]::Round($CodeCoverage.Statement.Executed / $CodeCoverage.Statement.Analyzed * 100, 2)
-
-    $TestResults.CodeCoverage.HitCommands | Group-Object -Property Function | ForEach-Object {
-        if (-Not $CodeCoverage.Functions.ContainsKey($_.Name)) {
-            $CodeCoverage.Functions.Add($_.Name, @{
-                Name     = $_.Name
-                Analyzed = 0
-                Executed = 0
-                Missed   = 0
+    if ($null -ne $TestResults) {
+        $CodeCoverage = @{
+            Functions = @{}
+            Statement = @{
+                Analyzed = $TestResults.CodeCoverage.NumberOfCommandsAnalyzed
+                Executed = $TestResults.CodeCoverage.NumberOfCommandsExecuted
+                Missed   = $TestResults.CodeCoverage.NumberOfCommandsMissed
                 Coverage = 0
-            })
+            }
+            Function = @{}
         }
-
-        $CodeCoverage.Functions[$_.Name].Analyzed += $_.Count
-        $CodeCoverage.Functions[$_.Name].Executed += $_.Count
-    }
-    $TestResults.CodeCoverage.MissedCommands | Group-Object -Property Function | ForEach-Object {
-        if (-Not $CodeCoverage.Functions.ContainsKey($_.Name)) {
-            $CodeCoverage.Functions.Add($_.Name, @{
-                Name     = $_.Name
-                Analyzed = 0
-                Executed = 0
-                Missed   = 0
-                Coverage = 0
-            })
+        $CodeCoverage.Statement.Coverage = [math]::Round($CodeCoverage.Statement.Executed / $CodeCoverage.Statement.Analyzed * 100, 2)
+    
+        $TestResults.CodeCoverage.HitCommands | Group-Object -Property Function | ForEach-Object {
+            if (-Not $CodeCoverage.Functions.ContainsKey($_.Name)) {
+                $CodeCoverage.Functions.Add($_.Name, @{
+                    Name     = $_.Name
+                    Analyzed = 0
+                    Executed = 0
+                    Missed   = 0
+                    Coverage = 0
+                })
+            }
+    
+            $CodeCoverage.Functions[$_.Name].Analyzed += $_.Count
+            $CodeCoverage.Functions[$_.Name].Executed += $_.Count
         }
-
-        $CodeCoverage.Functions[$_.Name].Analyzed += $_.Count
-        $CodeCoverage.Functions[$_.Name].Missed   += $_.Count
-    }
-    foreach ($function in $CodeCoverage.Functions.Values) {
-        $function.Coverage = [math]::Round($function.Executed / $function.Analyzed * 100)
-    }
-    $CodeCoverage.Function = @{
-        Analyzed = $CodeCoverage.Functions.Count
-        Executed = ($CodeCoverage.Functions.Values | Where-Object { $_.Executed -gt 0 }).Length
-        Missed   = ($CodeCoverage.Functions.Values | Where-Object { $_.Executed -eq 0 }).Length
-    }
-    $CodeCoverage.Function.Coverage = [math]::Round($CodeCoverage.Function.Executed / $CodeCoverage.Function.Analyzed * 100, 2)
+        $TestResults.CodeCoverage.MissedCommands | Group-Object -Property Function | ForEach-Object {
+            if (-Not $CodeCoverage.Functions.ContainsKey($_.Name)) {
+                $CodeCoverage.Functions.Add($_.Name, @{
+                    Name     = $_.Name
+                    Analyzed = 0
+                    Executed = 0
+                    Missed   = 0
+                    Coverage = 0
+                })
+            }
     
-    #endregion
-
-    # Define thresholds for pass / fail dont actually fail the build though yet, just for more info
-    $StatementThreshold = 80
-    $FunctionThreshold = 100
+            $CodeCoverage.Functions[$_.Name].Analyzed += $_.Count
+            $CodeCoverage.Functions[$_.Name].Missed   += $_.Count
+        }
+        foreach ($function in $CodeCoverage.Functions.Values) {
+            $function.Coverage = [math]::Round($function.Executed / $function.Analyzed * 100)
+        }
+        $CodeCoverage.Function = @{
+            Analyzed = $CodeCoverage.Functions.Count
+            Executed = ($CodeCoverage.Functions.Values | Where-Object { $_.Executed -gt 0 }).Length
+            Missed   = ($CodeCoverage.Functions.Values | Where-Object { $_.Executed -eq 0 }).Length
+        }
+        $CodeCoverage.Function.Coverage = [math]::Round($CodeCoverage.Function.Executed / $CodeCoverage.Function.Analyzed * 100, 2)
+        
+        #endregion
     
-    "`n`tStatement coverage: $($CodeCoverage.Statement.Analyzed) analyzed, $($CodeCoverage.Statement.Executed) executed, $($CodeCoverage.Statement.Missed) missed, $($CodeCoverage.Statement.Coverage)%."
-    if ($CodeCoverage.Statement.Coverage -ge $StatementThreshold) {
-        # passed Statement coverage test
-        Write-Host "`tPassed statement coverage threshold of: $StatementThreshold%" -ForegroundColor Green
-        Update-AppveyorTest -Name "PesterStatementCoverage" -Outcome Passed
+        # Define thresholds for pass / fail dont actually fail the build though yet, just for more info
+        $StatementThreshold = 80
+        $FunctionThreshold = 100
+        
+        "`n`tStatement coverage: $($CodeCoverage.Statement.Analyzed) analyzed, $($CodeCoverage.Statement.Executed) executed, $($CodeCoverage.Statement.Missed) missed, $($CodeCoverage.Statement.Coverage)%."
+        if ($CodeCoverage.Statement.Coverage -ge $StatementThreshold) {
+            # passed Statement coverage test
+            Write-Host "`tPassed statement coverage threshold of: $StatementThreshold%" -ForegroundColor Green
+            Update-AppveyorTest -Name "PesterStatementCoverage" -Outcome Passed
+        } else {
+            # failed Statement coverage test
+            Write-Warning "`tFailed function coverage threshold of: $StatementThreshold%"
+            Add-AppveyorMessage -Message "`tFailed function coverage threshold of: $StatementThreshold%`n" -Category Error
+            Update-AppveyorTest -Name "PesterStatementCoverage" -Outcome Failed -ErrorMessage "Pester statement coverage did not meet threshold of $StatementThreshold%"
+            # Failing the build
+            Throw "Build failed"
+        }
+        "`n`tFunction coverage: $($CodeCoverage.Function.Analyzed) analyzed, $($CodeCoverage.Function.Executed) executed, $($CodeCoverage.Function.Missed) missed, $($CodeCoverage.Function.Coverage)%."
+        if ($CodeCoverage.Function.Coverage -ge $FunctionThreshold) {
+            # passed Function coverage test
+            Write-Host "`tPassed function coverage threshold of: $FunctionThreshold%" -ForegroundColor Green
+            Update-AppveyorTest -Name "PesterFunctionCoverage" -Outcome Passed
+        } else {
+            # failed Function coverage test
+            Write-Warning "`tFailed function coverage threshold of: $FunctionThreshold%"
+            Add-AppveyorMessage -Message "`tFailed function coverage threshold of: $FunctionThreshold%`n" -Category Error
+            Update-AppveyorTest -Name "PesterFunctionCoverage" -Outcome Failed -ErrorMessage "Pester statement coverage did not meet threshold of $FunctionThreshold%"
+            # Failing the build
+            Throw "Build failed"
+        }
     } else {
-        # failed Statement coverage test
-        Write-Warning "`tFailed function coverage threshold of: $StatementThreshold%"
-        Add-AppveyorMessage -Message "`tFailed function coverage threshold of: $StatementThreshold%`n" -Category Error
-        Update-AppveyorTest -Name "PesterStatementCoverage" -Outcome Failed -ErrorMessage "Pester statement coverage did not meet threshold of $StatementThreshold%"
-        # Failing the build
-        Throw "Build failed"
-    }
-    "`n`tFunction coverage: $($CodeCoverage.Function.Analyzed) analyzed, $($CodeCoverage.Function.Executed) executed, $($CodeCoverage.Function.Missed) missed, $($CodeCoverage.Function.Coverage)%."
-    if ($CodeCoverage.Function.Coverage -ge $FunctionThreshold) {
-        # passed Function coverage test
-        Write-Host "`tPassed function coverage threshold of: $FunctionThreshold%" -ForegroundColor Green
-        Update-AppveyorTest -Name "PesterFunctionCoverage" -Outcome Passed
-    } else {
-        # failed Function coverage test
-        Write-Warning "`tFailed function coverage threshold of: $FunctionThreshold%"
-        Add-AppveyorMessage -Message "`tFailed function coverage threshold of: $FunctionThreshold%`n" -Category Error
-        Update-AppveyorTest -Name "PesterFunctionCoverage" -Outcome Failed -ErrorMessage "Pester statement coverage did not meet threshold of $FunctionThreshold%"
-        # Failing the build
-        Throw "Build failed"
+        Write-Host "No tests = no problems!" -ForegroundColor Green
     }
 }
