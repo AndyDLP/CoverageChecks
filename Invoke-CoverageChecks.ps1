0117d8c5f9e36860f92f6e7387bff7670ba3f4a8
Andy de la Pole <andrew.delapole@bcpartners.com>
2019-04-12 19:03:06 +0100

    .

--- Invoke-CoverageChecks.ps1
+++ Invoke-CoverageChecks.ps1
@@ -1100,6 +1100,40 @@ foreach ($DC in $AllDomainControllersPS) {
                 $FailedTests = $DCDiagString | Select-String -Pattern 'failed test'
 
                 <#
+# http://www.anilerduran.com/index.php/2013/how-to-parse-dcdiag-output-with-powershell/
+
+Import-Module activedirectory
+$DCs = $(Get-ADDomainController -filter *).name
+$Results = New-Object System.Object
+$Report = @()
+
+Foreach ($DC in $DCs) 
+    {
+    $DCDIAG = dcdiag /s:$DC
+    $Results | Add-Member -name Server -Value $DC -Type NoteProperty -Force
+    Foreach ($Entry in $DCDIAG) 
+        {
+        Switch -Regex ($Entry) {
+            "Starting" {$Testname = ($Entry -replace ".*Starting test: ").Trim()}
+            "passed|failed" {If ($Entry -match "Passed") 
+                                {
+                                $TestStatus = "Passed"
+                                } Else {
+                                $TestStatus = "** Failed **"
+                                }
+                            }
+            }
+        If (($TestName -ne $null) -and ($TestStatus -ne $null)) 
+            {
+            $Results | Add-Member -Type NoteProperty -name $($TestName.Trim()) -Value $TestStatus -Force
+            }
+        }
+    $Report += $Results
+    }
+
+$Report | select Server, NetLogons, KccEvent, Replications, Services, SystemLog, SysVolCheck | ft * -AutoSize
+
+
                 $Report = @()
                 $DCDIAG = dcdiag.exe
                 $OutputObjectParams = @{
